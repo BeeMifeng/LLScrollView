4c577762be564e2fdb88f9c017106808891a0981
王政 <davidwang@trueyon.local>
2018-09-12 14:53:08 +0800

    Initial Commit

---
+++ NewsApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewsApp
+//
+//  Created by 王政 on 2018/9/12.
+//  Copyright © 2018年 lijialun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
